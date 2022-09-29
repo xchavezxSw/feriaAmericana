62a65cd0e42d2beb46618a58b46ac310eae0f13e
xchavezx <ccordoba@biometricpro.io>
2022-09-29 20:13:52 -0300

    asd

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,request,current_app, flash, jsonify, make_response, redirect, request, url_for
 app = Flask(__name__)
+import json
 import random
 @app.route("/")
 def index():
@@ -7,5 +8,6 @@ def index():
     fotos=['https://urbanluxury.b-cdn.net/wp-content/uploads/2022/09/URBAN_LUXURY-1031-683x1024.jpg']
     fot=random.choice(fotos)
     data=[str(num),'pantalon','es un pantalon','1','500',fot]
+    data=json.loads(json.dumps(data))
     return make_response(jsonify(data), 200)
 
