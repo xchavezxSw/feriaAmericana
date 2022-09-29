6a31b2c050bc96734afa16098e3a8b0b47fbdef7
xchavezx <ccordoba@biometricpro.io>
2022-09-29 19:44:24 -0300

    asd

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,current_app, flash, jsonify, make_response, redirect, request, url_for
 app = Flask(__name__)
 import random
-num = random.random()
+num = random.randint(1,100)
 @app.route("/")
 def index():
     fotos=['https://urbanluxury.b-cdn.net/wp-content/uploads/2022/09/URBAN_LUXURY-1031-683x1024.jpg']
