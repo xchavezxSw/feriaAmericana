3e2cf5f1c48d7a1780a219b9cf03212d1e346da1
xchavezx <ccordoba@biometricpro.io>
2022-09-30 12:48:48 -0300

    asd

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ CORS(app)
 @app.route("/")
 def index():
     num = random.randint(1, 100)
-    fotos=['https://urbanluxury.b-cdn.net/wp-content/uploads/2022/09/URBAN_LUXURY-1031-683x1024.jpg']
-    fot=random.choice(fotos)
-    data={"id":str(num),'nombre':'pantalon','descripcion':'es un pantalon','stock':'1','precio':'500','foto':fot}
     output=[]
-    output.append(data)
+    output.append({"id":str(num),'nombre':'pantalon','descripcion':'Pantalon de Jean deportivo','stock':'1','precio':'500','foto':'https://urbanluxury.b-cdn.net/wp-content/uploads/2022/09/URBAN_LUXURY-1031-683x1024.jpg','categoria':'mujer'})
+    num = random.randint(1, 100)
+    output.append({"id":str(num),'nombre':'pantalon','descripcion':'Pantalon de Jean deportivo','stock':'1','precio':'500','foto':"https://urbanluxurymarcas.com/wp-content/uploads/2022/09/URBAN_LUXURY-291-500x750.jpg",'categoria':'hombres'})
+    num = random.randint(1, 100)
+    output.append({"id":str(num),'nombre':'pantalon','descripcion':'Pantalon de Jean deportivo','stock':'1','precio':'500','foto':"https://urbanluxurymarcas.com/wp-content/uploads/2022/08/URBAN_LUXURY-1471-500x750.jpg",'categoria':'ninos'})
     data=json.loads(json.dumps(output).encode('utf-8').decode('ascii'))
     return make_response(jsonify(data), 200)
 
