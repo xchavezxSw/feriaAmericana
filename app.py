166631e650564f3a39db9d552c6b02e191cd115f
xchavezx <ccordoba@biometricpro.io>
2022-09-30 12:49:43 -0300

    asd

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ CORS(app)
 def index():
     num = random.randint(1, 100)
     output=[]
-    output.append({"id":str(num),'nombre':'pantalon','descripcion':'Pantalon de Jean deportivo','stock':'1','precio':'500','foto':'https://urbanluxury.b-cdn.net/wp-content/uploads/2022/09/URBAN_LUXURY-1031-683x1024.jpg','categoria':'mujer'})
+    output.append({"id":str(num),'nombre':'Jean','descripcion':'Pantalon de Jean deportivo','stock':'1','precio':'500','foto':'https://urbanluxury.b-cdn.net/wp-content/uploads/2022/09/URBAN_LUXURY-1031-683x1024.jpg','categoria':'mujer'})
     num = random.randint(1, 100)
-    output.append({"id":str(num),'nombre':'pantalon','descripcion':'Pantalon de Jean deportivo','stock':'1','precio':'500','foto':"https://urbanluxurymarcas.com/wp-content/uploads/2022/09/URBAN_LUXURY-291-500x750.jpg",'categoria':'hombres'})
+    output.append({"id":str(num),'nombre':'Pantalon gabardina','descripcion':'Pantalon de gabardina','stock':'1','precio':'500','foto':"https://urbanluxurymarcas.com/wp-content/uploads/2022/09/URBAN_LUXURY-291-500x750.jpg",'categoria':'hombres'})
     num = random.randint(1, 100)
-    output.append({"id":str(num),'nombre':'pantalon','descripcion':'Pantalon de Jean deportivo','stock':'1','precio':'500','foto':"https://urbanluxurymarcas.com/wp-content/uploads/2022/08/URBAN_LUXURY-1471-500x750.jpg",'categoria':'ninos'})
+    output.append({"id":str(num),'nombre':'zapatillas','descripcion':'zapatillas para el cali','stock':'1','precio':'500','foto':"https://urbanluxurymarcas.com/wp-content/uploads/2022/08/URBAN_LUXURY-1471-500x750.jpg",'categoria':'ninos'})
     data=json.loads(json.dumps(output).encode('utf-8').decode('ascii'))
     return make_response(jsonify(data), 200)
 
